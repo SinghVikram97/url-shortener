89eba0fecdc38a763fb7cb00083fef1136959c67
singhVikram97 <vikram.bedi97@gmail.com>
2018-05-24 08:34:04 +0530

    Added copying

--- public/Js/index.js
+++ public/Js/index.js
@@ -22,8 +22,8 @@ $(document).ready(function () {
 
             shortUrl='http://localhost:4444/'+shortUrl;
 
-            let heading='<h4 id="shortLink" class="mr-2">'+shortUrl+'</h4>';
-            let btn='<button id="copy" class="btn btn-lg animated pulse"><i class="fas fa-copy mr-2" ></i>Copy</button>';
+            let heading='<h4 id="shortLink" class="mr-2" data-clipboard-target="#shortLink">'+shortUrl+'</h4>';
+            let btn='<button id="copy" class="btn btn-lg animated" data-clipboard-target="#shortLink"><i class="fas fa-copy mr-2" ></i>Copy</button>';
 
             copyDiv.append(heading);
             copyDiv.append(btn);
@@ -32,6 +32,34 @@ $(document).ready(function () {
 
     });
 
+    $(document).on('click','#copy',function(){
+
+        let clipboard= new ClipboardJS('#copy');
+        clipboard.on('success', function(e) {
+           console.log('Text copied into clipboard is: <' + e.text + '>');
+            e.clearSelection();
+        });
+
+    });
+
+    $(document).on('click','#shortLink',function () {
+
+        let clipboard= new ClipboardJS('#shortLink');
+        clipboard.on('success', function(e) {
+            console.log('Text copied into clipboard is: <' + e.text + '>');
+            e.clearSelection();
+        });
+
+    });
+
+    $(document).on('mouseenter','#copy', function (event) {
+
+        $(this).toggleClass('pulse');
+
+    }).on('mouseleave','.top-level',  function(){
+
+        $(this).toggleClass('pulse');
+    });
 
 
 });
