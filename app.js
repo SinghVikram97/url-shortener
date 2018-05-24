2f96ad5258c9ab205242ab0fd770af6d4e34c9df
singhVikram97 <vikram.bedi97@gmail.com>
2018-05-24 17:20:06 +0530

    Linked mlab

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shortUrl').then(function () {
+mongoose.connect('mongodb://vikram:urlpass@ds233970.mlab.com:33970/shorturl' || 'mongodb://localhost:27017/shortUrl').then(function () {
 
     console.log('Database connected');
 
